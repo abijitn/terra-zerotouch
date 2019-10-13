f00479bee320dd764f1976ac0764ef0ae05051ad
Abijit Gowda <an48939@Abijits-MacBook-Pro.local>
2019-10-13 02:04:39 -0400

    adding sqs-hardcoded

--- msg_lambda.py
+++ msg_lambda.py
@@ -1,5 +1,14 @@
+import boto3  
+import json
+
 def handler(event, context):
     return { "message": "Hello, World!" }
 
 def post_handler(event, context):
-    return { "message": "I should have created something..." }
+#    return { "message": "I should have created something..." }
+
+    sqs = boto3.resource('sqs')
+
+    queue = sqs.get_queue_by_name(QueueName='testsqs')
+
+    response = queue.send_message(MessageBody=json.dumps(event))
